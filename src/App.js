c331ed76118694fd7ce9dff554a6de61025b5c47
Rohit <rohit.sharma2@hexaviewtech.com>
2022-04-13 18:55:22 +0530

    Creating About.js and enable dark mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import TextForm from './Components/TextForm';
 import NavBar from './Components/NavBar';
+import About from './Components/About';
 
 function App() {
   return (
     <>
-    <NavBar/>
-    <TextForm/>
+   {/* <NavBar/> */}
+    {/*<TextForm/> */}
+    <About />
     </>
     
   );
