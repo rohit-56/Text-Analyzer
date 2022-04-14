4aef9cfb50cbbefbe7ea1a1e6d48c02cb226169f
Rohit <rohit.sharma2@hexaviewtech.com>
2022-04-14 18:55:40 +0530

    Creating state for enable dark mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import './App.css';
 import TextForm from './Components/TextForm';
 import NavBar from './Components/NavBar';
-import About from './Components/About';
+// import About from './Components/About';
+import React,{useState} from 'react';
 
 function App() {
+
+  const [mode,setMode] = useState('light');
+  const [textcolor ,setTextcolor] =useState('black');
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+     setMode('dark');
+     setTextcolor('white');
+    }
+    else{
+      setMode('light');
+      setTextcolor('black');
+    }
+  }
+
   return (
     <>
-   {/* <NavBar/> */}
-    {/*<TextForm/> */}
-    <About />
+   <NavBar title="TextUtils" mode={mode}  toggleMode={toggleMode} textcolor={textcolor} />
+    <TextForm/>
+    {/* <About /> */}
     </>
     
   );
