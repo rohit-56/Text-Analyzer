25ca55c5deac20833968988b2d6be7dad240b440
Rohit <rohit.sharma2@hexaviewtech.com>
2022-04-25 18:06:37 +0530

    Implement count characters function and button

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -21,13 +21,17 @@ export default function TextForm(props) {
   const convertLowerCase=()=>{
     setOutputText(text.toLowerCase());
   }
+
+  const countCharacter=()=>{
+    setOutputText(`${text.length}`-`${text.split(" ").length}`+1);
+  }
   const [text, setText] = useState('Enter Text Here');
   const [outputText,setOutputText]=useState("");
 
   
   return (
     <>
-    <div className="container my-3" style={{color : props.textcolor, border:props.textcolor}}>
+    <div className="container my-3" style={{color : props.textcolor}}>
     
        <h1>Enter your text</h1>
        <div className="mb-3">
@@ -37,6 +41,9 @@ export default function TextForm(props) {
         <button className="btn btn-primary mx-1" onClick={convertLowerCase}>Convert your text into LowerCase</button>
         <button className="btn btn-primary mx-1" onClick={clearAll}>Clear Text</button>
         <button className="btn btn-primary mx-1" onClick={countWords}>Count Words</button>
+        <button className="btn btn-primary mx-1" onClick={countCharacter}>Count Characters</button>
+        <button className="btn btn-primary mx-1" onClick={countWords}>Count Digits</button>
+        <button className="btn btn-primary mx-1" onClick={countWords}>Count Special Characters</button>
     </div>
     <div className='container' style={{color:props.textcolor}}> 
       <h1>Output</h1>
